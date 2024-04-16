0248851ef7cd84864f67cb75ad62f2f950718b12
Caio Fernando <caiofsr13@gmail.com>
2024-04-16 18:52:34 -0300

    update echo colors

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 brew_install() {
-  echo -e "${PURPLE}###############\n Installing Homebrew\n###############"
+  echo -e "${GREEN}###############\n Installing Homebrew\n###############"
 
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
@@ -12,19 +12,19 @@ brew_install() {
 }
 
 install_depencies() {
-  echo -e "${PURPLE}###############\n Update apt\n###############"
+  echo -e "${GREEN}###############\n Update apt\n###############"
 
   sudo apt update
 
   sudo apt upgrade -y
 
-  echo -e "${PURPLE}###############\n Installing dependencies\n###############"
+  echo -e "${GREEN}###############\n Installing dependencies\n###############"
 
   sudo apt install -y wget curl zsh git build-essential unzip
 }
 
 clone_repo() {
-  echo -e "${PURPLE}###############\n Cloning repo\n###############"
+  echo -e "${GREEN}###############\n Cloning repo\n###############"
 
   git clone https://github.com/caiofsr/setup-new-pc.git .setup-pc
 
@@ -32,7 +32,7 @@ clone_repo() {
 }
 
 install_oh_my_zsh() {
-  echo -e "${PURPLE}###############\n Installing oh my zsh\n###############"
+  echo -e "${GREEN}###############\n Installing oh my zsh\n###############"
 
   curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
@@ -44,27 +44,27 @@ install_oh_my_zsh() {
 }
 
 install_asdf() {
-  echo -e "${PURPLE}###############\n Installing asdf\n###############"
+  echo -e "${GREEN}###############\n Installing asdf\n###############"
 
   brew install asdf
 
   source ~/.zshrc
 
-  echo -e "${PURPLE}###############\n Installing NodeJS with asdf\n###############"
+  echo -e "${GREEN}###############\n Installing NodeJS with asdf\n###############"
   asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
   asdf install nodejs latest
 
-  echo -e "${PURPLE}###############\n Installing Go with asdf\n###############"
+  echo -e "${GREEN}###############\n Installing Go with asdf\n###############"
   asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
   asdf install golang latest
 
-  echo -e "${PURPLE}###############\n Installing bun with asdf\n###############"
+  echo -e "${GREEN}###############\n Installing bun with asdf\n###############"
   asdf plugin add bun
   asdf install bun latest
 }
 
 add_spaceships_to_zsh() {
-  echo -e "${PURPLE}###############\n Cloning spaceship-theme\n###############"
+  echo -e "${GREEN}###############\n Cloning spaceship-theme\n###############"
 
   git clone https://github.com/spaceship-prompt/spaceship-prompt.git "~/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
 
@@ -72,7 +72,7 @@ add_spaceships_to_zsh() {
 }
 
 post_install() {
-  echo -e "${PURPLE}###############\n Running post install commands\n###############"
+  echo -e "${GREEN}###############\n Running post install commands\n###############"
 
   zsh
 
